5198c5fe2c4971ba021e9dab4f8948a4a24f88f8
cloudbees <cloudbees@localhost.localdomain>
2018-11-13 15:57:33 -0500

    test

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,7 +1,10 @@
 from junit_xml import TestSuite, TestCase
 
 print( "Goodbye, World!" )
+
 test_case = [TestCase('Test1', 'some.class.name', 123.345, 'I am stdout!', 'I am stderr!')]
 ts = TestSuite("my test suite", test_case)
 print(TestSuite.to_xml_string([ts]))
+with open('output.xml', 'w') as f:
+  TestSuite.to_file(f, [ts], prettyprint=false)
 
